f9e21c553c614208864f6330471c09d91ea28d82
Michael Hu <43020369+GuessHuIAm@users.noreply.github.com>
2023-12-04 12:54:38 -0500

    two transformers

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from transformer import transformer_api
+from transformer import masked_transformer_api, standard_transformer_api
 import time
 
 app = Flask(__name__)
@@ -11,15 +11,22 @@ def index():
 @app.route('/classify', methods=['POST'])
 def classify_text():
     input_text = request.form['text']
+    
     start_time = time.time()
-    result, matrix = transformer_api(input_text)
+    masked_result, masked_matrix = masked_transformer_api(input_text)
     end_time = time.time()
+    masked_duration = end_time - start_time  # Calculate duration
+    masked_matrix_list = masked_matrix 
+    
+    start_time = time.time()
+    standard_result, standard_matrix = standard_transformer_api(input_text)
+    end_time = time.time()
+    standard_duration = end_time - start_time  # Calculate duration
+    standard_matrix_list = standard_matrix
 
-    duration = end_time - start_time  # Calculate duration
-
-    matrix_list = matrix 
-
-    return render_template('result.html', text=input_text, classification=result, time_taken=duration, matrix=matrix_list)
+    return render_template('result.html', text=input_text,
+                        masked_classification=masked_result, masked_time_taken=masked_duration, masked_matrix=masked_matrix_list,
+                        standard_classification=standard_result, standard_time_taken=standard_duration, standard_matrix=standard_matrix_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- transformer.py
+++ transformer.py
@@ -2,14 +2,17 @@ from transformers import BertTokenizer, BertForSequenceClassification
 import torch
 import json
 
-# Assuming 'emailspam' folder is in the current working directory
-# Update the path if 'emailspam' is located elsewhere
-model_path = 'emailspam'
-tokenizer_path = 'emailspam'
-common_tokens_path = 'emailspam/common_tokens.json'
+# Assuming 'masked' folder is in the current working directory
+masked_model_path = 'masked'
+tokenizer_path = 'masked'
+common_tokens_path = 'masked/common_tokens.json'
 
-# Load the model
-model = BertForSequenceClassification.from_pretrained(model_path)
+standard_model_path = 'standard'
+
+
+# Load the models
+masked_model = BertForSequenceClassification.from_pretrained(masked_model_path)
+standard_model = BertForSequenceClassification.from_pretrained(standard_model_path)
 
 # Load the tokenizer
 tokenizer = BertTokenizer.from_pretrained(tokenizer_path)
@@ -33,7 +36,7 @@ def create_modified_attention_mask(email, tokenizer, common_tokens, max_length=1
 
     return modified_attention_mask
 
-def transformer_api(text):
+def masked_transformer_api(text):
     # Tokenize the input text
     encoded_dict = tokenizer.encode_plus(
         text,
@@ -55,11 +58,11 @@ def transformer_api(text):
         mod_attention_mask_tensor = mod_attention_mask_tensor[:, :encoded_dict['input_ids'].size(1)]
 
     # Set model to evaluation mode
-    model.eval()
+    masked_model.eval()
 
     # Perform the prediction
     with torch.no_grad():
-        outputs = model(encoded_dict['input_ids'], token_type_ids=None, attention_mask=mod_attention_mask_tensor)
+        outputs = masked_model(encoded_dict['input_ids'], token_type_ids=None, attention_mask=mod_attention_mask_tensor)
         logits = outputs.logits
         prediction = torch.argmax(logits, dim=-1).numpy()
 
@@ -69,3 +72,29 @@ def transformer_api(text):
     # Return both classification and modified attention mask
     return classification_label, mod_attention_mask
 
+
+def standard_transformer_api(text):
+    # Tokenize the input text
+    encoded_dict = tokenizer.encode_plus(
+        text,
+        add_special_tokens=True,
+        max_length=128,
+        pad_to_max_length=True,
+        return_attention_mask=True,
+        return_tensors='pt'
+    )
+
+    # Set model to evaluation mode
+    standard_model.eval()
+
+    # Perform the prediction
+    with torch.no_grad():
+        outputs = standard_model(encoded_dict['input_ids'], token_type_ids=None, attention_mask=encoded_dict['attention_mask'])
+        logits = outputs.logits
+        prediction = torch.argmax(logits, dim=-1).numpy()
+
+    # Convert prediction to a readable format
+    classification_label = 'Spam' if prediction == 1 else 'Ham'
+
+    # Return both classification and modified attention mask
+    return classification_label, encoded_dict['attention_mask']
